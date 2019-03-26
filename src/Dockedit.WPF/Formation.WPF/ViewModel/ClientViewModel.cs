cbe93630a381d8c796f99e9557dbce79ae1ad279
Sonu Jose <sonujose993@gmail.com>
2019-03-26 11:23:11 +0530

    Adding mvvm wpf application

---
+++ src/Dockedit.WPF/Formation.WPF/ViewModel/ClientViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Formation.WPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Linq;
+
+namespace Formation.WPF
+{
+    public class ClientViewModel :CommandBase<Client>
+    {
+        private DemoEntities dbContext = new DemoEntities();
+
+        protected override void Get()
+        {
+            try
+            {
+                //dbContext.Client.ToList().ForEach(monClient => dbContext.Client.Local.Add(monClient));
+                //Collection = dbContext.Client.Local;
+            }
+            catch (Exception ex)
+            {      
+                throw ex;
+            }
+            
+        }
+        protected override bool CanGet()
+        {
+            return true;
+        }
+        protected override void Save()
+        {
+            foreach (Client item in Collection)
+            {
+                if (dbContext.Entry(item).State == System.Data.Entity.EntityState.Added)
+                {
+                    dbContext.Client.Add(item);
+                }
+            }
+            dbContext.SaveChanges();
+        }
+        protected override void Delete()
+        {
+
+        }
+    }
+}
+
